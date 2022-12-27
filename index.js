97ba23d25a13a5d86271bb096c86559590e162d8
areain10 <107684386+areain10@users.noreply.github.com>
2022-12-27 14:02:50 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -31,4 +31,55 @@ window.addEventListener('DOMContentLoaded',()=>{
 	setTimeout(()=>{
 		intro.style.top= '-100vh';
 	},4000);
-})
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+let aboutcard = document.querySelector('#aboutcard');
+let portcard = document.querySelector('#portcard');
+let servicecard = document.querySelector('#servicecard');
+let contactcard = document.querySelector('#contactcard');
+aboutcard.addEventListener('mouseover',()=>{
+	setTimeout(()=>{
+
+		document.querySelector('#abouttext').classList.add('active');
+	});
+});
+
+aboutcard.addEventListener('mouseout',()=>{
+	document.querySelector('#abouttext').classList.remove('active');
+	document.querySelector('#abouttext').classList.add('fade');
+});
+portcard.addEventListener('mouseover',()=>{
+	document.querySelector('#porttext').classList.add('active');
+});
+
+portcard.addEventListener('mouseout',()=>{
+	document.querySelector('#porttext').classList.remove('active');
+	document.querySelector('#porttext').classList.add('fade');
+});
+servicecard.addEventListener('mouseover',()=>{
+	document.querySelector('#servicetext').classList.add('active');
+});
+
+servicecard.addEventListener('mouseout',()=>{
+	document.querySelector('#servicetext').classList.remove('active');
+	document.querySelector('#servicetext').classList.add('fade');
+});
+contactcard.addEventListener('mouseover',()=>{
+	document.querySelector('#contacttext').classList.add('active');
+});
+
+contactcard.addEventListener('mouseout',()=>{
+	document.querySelector('#contacttext').classList.remove('active');
+	document.querySelector('#contacttext').classList.add('fade');
+});
